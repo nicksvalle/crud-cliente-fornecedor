ce263ad3b43ea520c2dd6ce1a8fc5de6da617f51
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2023-05-19 16:04:58 -0300

    finalizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './home/home.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores.component';
+import { ClientesComponent } from './clientes/clientes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'fornecedores', component: FornecedoresComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
